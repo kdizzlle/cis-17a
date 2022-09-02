3abb0c5e505aa62674ea7fb997cd6ee34d2fbddb
kelly williams <kelly.mary.williams@gmail.com>
2022-09-01 18:07:02 -0700

    Added Hello World Project to Labs folder

---
+++ Labs/Hello World/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: 2790755
+ *
+ * Created on September 1, 2022, 6:03 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World!";
+    return 0;
+}
+
